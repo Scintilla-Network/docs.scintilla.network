b0db0313cba3c7dfa7a49b5586293af7b3057cae
Alex Werner <obusco@gmail.com>
2024-02-10 19:37:34 +0100

    feat: provide improved doc

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,27 +1,27 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import {DocsThemeConfig} from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Scintilla-Network: Protocol and Usage Documentation</span>,
-  project: {
-    link: 'https://scintilla.network',
-  },
-  chat: {
-    link: 'https://discord.gg/3kPvEWNrXE',
-  },
-  docsRepositoryBase: 'https://github.com/Scintilla-Network/docs.scintilla.network',
-  footer: {
-    text: 'Scintilla Network Documentation',
-  },
-  head: (
-      <>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Scintilla-Network: Protocol and Usage Documentation" />
-        <meta name="og:title" content="Scintilla-Network: Protocol and Usage Documentation" />
-        <meta property="og:suffix" content="- Scintilla-Network" />
-      </>
-  ),
-  useNextSeoProps: () => ({ titleTemplate: '%s – Scintilla-Network' }),
+    logo: <span>Scintilla-Network: Protocol and Usage Documentation</span>,
+    project: {
+        link: 'https://scintilla.network',
+    },
+    chat: {
+        link: 'https://discord.gg/3kPvEWNrXE',
+    },
+    docsRepositoryBase: 'https://github.com/Scintilla-Network/docs.scintilla.network',
+    footer: {
+        text: 'Scintilla Network Documentation. Modular, Scalable, Secure, Open and trustless network for the future of RWAs'
+    },
+    head: (
+        <>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <meta name="description" content="Scintilla-Network: Protocol and Usage Documentation"/>
+            <meta name="og:title" content="Scintilla-Network: Protocol and Usage Documentation"/>
+            <meta property="og:suffix" content="- Scintilla-Network"/>
+        </>
+    ),
+    useNextSeoProps: () => ({titleTemplate: '%s – Scintilla-Network'}),
 }
 
 export default config
